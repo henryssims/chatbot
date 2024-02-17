74ba4f8d9d5ca66007588493ca11c37f38a16a05
henryssims <134118548+henryssims@users.noreply.github.com>
2024-02-16 23:56:08 -0500

    removed commented code

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ import readlineSync from 'readline-sync';
 import colors from 'colors';
 
 async function main() {
-    // const chatCompletion = await openai.chat.completions.create({
-    //     model: 'gpt-3.5-turbo',
-    //     messages: [
-    //         { role: 'user', content: 'What is the capital of Pennsylvania?'}
-    //     ]
-    // });
-
-    // console.log(chatCompletion.choices[0].message.content);
-
     console.log(colors.bold.green('Welcome to the Chatbot Program'));
     console.log(colors.bold.green('You can start chatting with the bot'));
 
